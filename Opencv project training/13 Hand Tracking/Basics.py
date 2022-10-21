6bfe58ff1d139ebb41561030bfc7c0f418db9584
Auorui <100789256+Auorui@users.noreply.github.com>
2022-10-21 11:03:55 +0800

    Add files via upload

---
+++ Opencv project training/13 Hand Tracking/Basics.py
@@ -0,0 +1,43 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+#hands这个类只使用了RGB
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0  #previous time
+cTime = 0  #current time
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 8:
+                cv2.circle(img, (cx, cy), 12, (255, 0, 100), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (12, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    k=cv2.waitKey(1)
+    if k==27:
+        break
---
+++ Opencv project training/13 Hand Tracking/Project_4_thumb.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img, draw=True )
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4])
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    #cv2.putText(img, str(int(fps)), (12, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+    #                (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    k=cv2.waitKey(1)
+    if k==27:
+        break
