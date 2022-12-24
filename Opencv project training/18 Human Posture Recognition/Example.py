31126a3c495fa2ab5ea7b716d9cec32807cd70fe
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2022-12-24 22:49:32 +0800

    Add files via upload

---
+++ Opencv project training/18 Human Posture Recognition/Example.py
@@ -0,0 +1,26 @@
+import cv2
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture('Pose_videos/02.mp4')
+pTime = 0
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) !=0:
+        print(lmList[14])
+        cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
